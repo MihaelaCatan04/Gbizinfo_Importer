7e66275ad37b44da4b89ae7268146eb297f7da98
Mihaela Catan <mihaelacatan7@gmail.com>
2026-04-30 13:12:02 +0300

    fix: Correct security

--- src/main/java/com/java/importer/config/OpenApiConfig.java
+++ src/main/java/com/java/importer/config/OpenApiConfig.java
@@ -1,6 +1,8 @@
 package com.java.importer.config;
 
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -8,8 +10,11 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+
+    private static final String SCHEME_NAME = "basicAuth";
+
     @Bean
-    public io.swagger.v3.oas.models.OpenAPI customOpenAPI() {
-        return new io.swagger.v3.oas.models.OpenAPI().info(new io.swagger.v3.oas.models.info.Info().title("Importer Executor").version("1.0").description("API documentation for the Importer Microservice.")).addSecurityItem(new SecurityRequirement().addList("ImporterScheme")).components(new Components().addSecuritySchemes("Importer", new SecurityScheme().name("JavaTestScheme").type(SecurityScheme.Type.HTTP).scheme("basic")));
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info().title("Importer Executor").version("1.0").description("API documentation for the Importer Microservice.")).addSecurityItem(new SecurityRequirement().addList(SCHEME_NAME)).components(new Components().addSecuritySchemes(SCHEME_NAME, new SecurityScheme().name(SCHEME_NAME).type(SecurityScheme.Type.HTTP).scheme("basic")));
     }
 }
--- src/main/java/com/java/importer/config/SecurityConfig.java
+++ src/main/java/com/java/importer/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth -> auth.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll().requestMatchers("/executor/trigger").authenticated().anyRequest().authenticated()).exceptionHandling(exception -> exception.authenticationEntryPoint((_, response, _) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Access Denied"))).httpBasic(Customizer.withDefaults());
+        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth -> auth.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll().anyRequest().authenticated()).exceptionHandling(exception -> exception.authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Access Denied"))).httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
@@ -36,7 +36,6 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
         UserDetails user = User.withUsername(username).password(passwordEncoder.encode(password)).roles("ADMIN").build();
-
         return new InMemoryUserDetailsManager(user);
     }
 
